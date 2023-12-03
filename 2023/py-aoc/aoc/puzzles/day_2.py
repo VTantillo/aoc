606644006644076b94fbbe3ec98a679e1178134d
Victor Tantillo <v.tantillo3@gmail.com>
2023-12-03 10:39:48 -0700

    Day 2 pt 2
    Added the day 3 puzzle text

--- 2023/py-aoc/aoc/puzzles/day_2.py
+++ 2023/py-aoc/aoc/puzzles/day_2.py
@@ -24,8 +24,42 @@ class CubeGameTotals(BaseModel):
     totals: CubeNumbers
 
 
-def day_2(input: list[str], params: Day2Params):
+def day_2(input: list[str]) -> int:
     games = parse_input(input)
+    answer = part_2(games)
+    return answer
+
+
+def part_2(games: list[CubeGame]) -> int:
+    powers: list[int] = []
+    for g in games:
+        answer = calc_game_power(g)
+        powers.append(answer)
+
+    sum = 0
+    for p in powers:
+        sum += p
+    return sum
+
+
+def calc_game_power(game: CubeGame) -> int:
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+    for g in game.games:
+        if g.red_cubes > min_red:
+            min_red = g.red_cubes
+
+        if g.green_cubes > min_green:
+            min_green = g.green_cubes
+
+        if g.blue_cubes > min_blue:
+            min_blue = g.blue_cubes
+
+    return min_red * min_green * min_blue
+
+
+def part_1(games: list[CubeGame], params: Day2Params) -> int:
     totals: list[CubeGameTotals] = []
 
     for game in games:
--- 2023/py-aoc/main.py
+++ 2023/py-aoc/main.py
@@ -1,8 +1,9 @@
-from aoc.puzzles.day_2 import Day2Params, day_2
+from aoc.puzzles.day_2 import day_2
 
 from aoc.utils.util import read_input
 
 
 input = read_input("../inputs/day2.txt")
-result = day_2(input, Day2Params(red_cubes=12, green_cubes=13, blue_cubes=14))
+# result = day_2(input, Day2Params(red_cubes=12, green_cubes=13, blue_cubes=14))
+result = day_2(input)
 print("result", result)
