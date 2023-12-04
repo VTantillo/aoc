9e0a61b25a46bed6975b3d984e7470aab252b751
Victor Tantillo <victor.tantillo@lument.com>
2023-12-04 14:26:00 -0700

    Closer

--- 2023/py-aoc/aoc/puzzles/day_3.py
+++ 2023/py-aoc/aoc/puzzles/day_3.py
@@ -19,9 +19,8 @@ def day_3(input: list[str]) -> int:
                 is_in_number = False
             elif is_digit(char) and not is_in_number:
                 is_in_number = True
-                part_num = find_part_number(symbols, input, l_num, c_num)
-                if part_num != -1:
-                    line_numbers.append(part_num)
+                ratio = find_gear_ratios(symbols, input, l_num, c_num)
+                line_numbers.append(ratio)
         part_numbers.extend(line_numbers)
 
     sum = 0
@@ -85,3 +84,121 @@ def find_part_number(
                 return num
 
     return -1
+
+
+def find_gear_ratios(
+    symbols: set[str], input: list[str], line_num: int, char_num: int
+) -> int:
+    num, num_start, num_end = find_number(symbols, input, line_num, char_num)
+
+    first_row = line_num == 0
+    last_row = line_num == len(input) - 1
+    first_col = num_start == 0
+    last_col = num_end == len(input[line_num]) - 1
+
+    for i in range(char_num, num_end + 1):
+        if not first_row and input[line_num - 1][i] == "*":
+            return find_other_ratio(symbols, input, line_num - 1, i, num)
+        if not last_row and input[line_num + 1][i] == "*":
+            return find_other_ratio(symbols, input, line_num + 1, i, num)
+        if i == char_num:
+            if not first_row and not first_col and input[line_num - 1][i - 1] == "*":
+                return find_other_ratio(symbols, input, line_num - 1, i - 1, num)
+            if not last_row and not first_col and input[line_num + 1][i - 1] == "*":
+                return find_other_ratio(symbols, input, line_num + 1, i - 1, num)
+            if not first_col and input[line_num][i - 1] == "*":
+                return find_other_ratio(symbols, input, line_num, i - 1, num)
+        if i == num_end:
+            if not first_row and not last_col and input[line_num - 1][i + 1] == "*":
+                return find_other_ratio(symbols, input, line_num - 1, i + 1, num)
+            if not last_row and not last_col and input[line_num + 1][i + 1] == "*":
+                return find_other_ratio(symbols, input, line_num + 1, i + 1, num)
+            if not last_col and input[line_num][i + 1] == "*":
+                return find_other_ratio(symbols, input, line_num, i + 1, num)
+
+    return 0
+
+
+def find_other_ratio(
+    symbols: set[str], input: list[str], line_num: int, char_num: int, num: int
+) -> int:
+    first_row = line_num == 0
+    last_row = line_num == len(input) - 1
+    first_col = char_num == 0
+    last_col = char_num == len(input[line_num]) - 1
+
+    second_num = (0, 0, 0)
+
+    if not first_row and is_digit(input[line_num - 1][char_num]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not last_row and is_digit(input[line_num + 1][char_num]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not first_row and not first_col and is_digit(input[line_num - 1][char_num - 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not last_row and not first_col and is_digit(input[line_num + 1][char_num - 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not first_col and is_digit(input[line_num][char_num - 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not first_row and not last_col and is_digit(input[line_num - 1][char_num + 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not last_row and not last_col and is_digit(input[line_num + 1][char_num + 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+    if not last_col and is_digit(input[line_num][char_num + 1]):
+        second_num = find_number(symbols, input, line_num - 1, char_num)
+        if second_num[0] == num:
+            second_num = (0, 0, 0)
+
+    print(f"num: {num}, second: {second_num[0]}, ratio: {num*second_num[0]}")
+
+    return num * second_num[0]
+
+
+def find_number(
+    symbols: set[str], input: list[str], line_num: int, char_num: int
+) -> tuple[int, int, int]:
+    in_number = True
+    num_start = char_num
+    num_end = char_num
+
+    while in_number:
+        if (
+            input[line_num][num_start] == "."
+            or input[line_num][num_start] in symbols
+            or num_start == 0
+        ):
+            in_number = False
+        else:
+            num_start -= 1
+
+    in_number = True
+    while in_number:
+        if (
+            input[line_num][num_end] == "."
+            or input[line_num][num_end] in symbols
+            or num_end == len(input[line_num]) - 1
+        ):
+            in_number = False
+        else:
+            num_end += 1
+
+    if input[line_num][num_start] == "." or input[line_num][num_start] in symbols:
+        num_start += 1
+    num = int(input[line_num][num_start:num_end])
+    num_end -= 1
+    print(f"num start: {num_start}, num end: {num_end}, line num: {line_num} - {num}")
+
+    return (num, num_start, num_end)
--- 2023/py-aoc/main.py
+++ 2023/py-aoc/main.py
@@ -1,7 +1,7 @@
 from aoc.puzzles.day_3 import day_3
 from aoc.utils.util import read_input
 
-input = read_input("../inputs/day3.txt")
+input = read_input("../inputs/day3-ex.txt")
 result = day_3(input)
 # 1 answer: 527938 <- too low
 # 2 answer: 527946 <- won't tell me
